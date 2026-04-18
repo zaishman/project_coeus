d9692746a5456a9f692257f0c131f25ae9bc284f
Aditi Divakar <divakaraditi6@gmail.com>
2026-04-18 12:15:43 -0500

    Change gitignore settings to not show .env

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const searchEngineId = process.env.GOOGLE_SEARCH_ENGINE_ID
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.send("Server Running")
